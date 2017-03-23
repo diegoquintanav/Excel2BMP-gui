7c437b986f8954c8b29f923c81d7bee713857986
Diego Quintana <daquintanav@gmail.com>
2017-03-23 18:42:08 -0300

    default mouse sender

--- Excel2BMP-gui/MainWindow.xaml.cs
+++ Excel2BMP-gui/MainWindow.xaml.cs
@@ -1,28 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Excel2BMP_gui
+
+namespace WpfTutorialSamples.XAML
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class EventsSample : Window
     {
-        public MainWindow()
+        public EventsSample()
         {
             InitializeComponent();
+            pnlMainGrid.MouseUp += new MouseButtonEventHandler(pnlMainGrid_MouseUp);
+            pnlMainGrid.MouseDown += new MouseButtonEventHandler(pnlMainGrid_MouseUp);  
         }
+
+        private void pnlMainGrid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("You clicked me at " + e.GetPosition(this).ToString());
+        }
+
     }
 }
